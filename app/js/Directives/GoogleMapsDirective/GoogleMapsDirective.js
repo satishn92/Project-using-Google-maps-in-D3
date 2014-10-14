e0432215288196313c2336ebf015dc5e448e8b8c
satishn92 <satish@obelisksolutions.in>
2014-10-14 21:20:34 +0530

    Added google map and added eventlisteners to it

--- app/js/Directives/GoogleMapsDirective/GoogleMapsDirective.js
+++ app/js/Directives/GoogleMapsDirective/GoogleMapsDirective.js
@@ -5,7 +5,71 @@ function GoogleMapsDirective() {
   console.log("Welcome to google maps directive");
   return {
     restrict: 'EA',
+    scope: {
+      data: '='
+    },
     templateUrl: 'templates/GoogleMapsTemplate.html',
-    link: function($scope, $element, $attr) {}
+    link: function($scope, $element, $attr) {
+      console.log($scope.data);
+      var map = new google.maps.Map(d3.select("div.mapContainer").node(), {
+          zoom: 14,
+          center: new google.maps.LatLng(12.930748, 77.633042),
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+      });
+
+      var marker = new google.maps.Marker({
+        position: map.getCenter(),
+        map:map,
+        title: 'click to zoom'
+      });
+
+      google.maps.event.addListener(map, 'click', function(event) {
+        console.log(event.latLng);
+        if(placeMarker !== undefined) {
+          placeMarker(event.latLng);
+        }
+      });
+
+      function placeMarker(location) {
+        var addedMarker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+      }
+
+      // var overlay = new google.maps.OverlayView();
+
+      // overlay.onAdd = function() {
+      //   var layer = d3.select(this.getPanes().overlayClickTarget)
+      //                 .append("div")
+      //                 .attr("class", "stations");
+
+      //   layer
+      //   .on("click", mapClick);
+      //   // .on("mouseover", mapMouseOver)
+      //   // .on("mousemove", mouseMove);
+
+      //   function mapClick(d) {
+      //     console.log(d);
+      //   }
+
+      //   function mapMouseOver(d) {
+      //     console.log(d);
+      //   }
+
+      //   function mouseMove(d) {
+      //     console.log(d);
+      //   }
+      // }
+
+      // overlay.setMap(map);
+
+      // overlay
+      // .on("click", mapClick)
+      // .on("mouseover", mapMouseOver)
+      // .on("mousemove", mouseMove);
+
+      
+    }
   };
 }
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,6 +2,10 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', [ 'settings' , '$scope', View1Ctrl]);
 
-}]);
+
+function View1Ctrl(settings, $scope) {
+  console.log("Welcome to controller");
+  $scope.data = settings.data;
+}
