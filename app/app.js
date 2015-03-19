640e0ea7969ee5bc683ae6191457398de6cfca23
satishn92 <satish@obelisksolutions.in>
2015-03-19 19:41:58 +0530

    Adds Gitignore file and Adds Brazil choropleth

--- app/app.js
+++ app/app.js
@@ -4,14 +4,22 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
+  'myApp.view2',
   'myApp.version',
   'app.directives',
   'app.settings'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.otherwise({
+    redirectTo: '/view1'
   });
+  $routeProvider
+    .when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    })
+    .when('/view2', {
+      templateUrl: 'view2/view2.html',
+      controller: 'View2Ctrl'
+    });
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,11 +1,9 @@
-'use strict';
-
 angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [ 'settings' , '$scope', View1Ctrl]);
 
 
 function View1Ctrl(settings, $scope) {
-  console.log("Welcome to controller");
+  console.log("Welcome to controller 1");
   $scope.data = settings.data;
 }
